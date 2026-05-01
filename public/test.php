df2b39ce2d98e32afed75dcf75158414e0ac05ef
Mauro Gs <maurodeveloper86@gmail.com>
2026-05-01 20:07:54 -0300

    Update test

--- public/test.php
+++ public/test.php
@@ -1,3 +1,4 @@
 <?php
-echo "Hello from Railway!";
-phpinfo();
+echo "PORT: " . getenv('PORT') . "\n";
+echo "RAILWAY_PUBLIC_DOMAIN: " . getenv('RAILWAY_PUBLIC_DOMAIN') . "\n";
+echo "Hello Railway!";
